621d68d2bd07111bfd838479dfe0fa34aef7eda3
Bronson Brown <Brsn90@icloud.com>
2020-01-25 02:39:22 -0700

    Update todos-list.js

--- src/components/todos-list.js
+++ src/components/todos-list.js
@@ -8,12 +8,12 @@ const Todo = props => (
         <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_name}</td>
         <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
         <td>
-            <Link to={"/edit/" + props.todo._id}>Edit</Link>
+            <Link to={"/todos/" + props.todo._id}>Edit</Link>
 
         </td>
         <td>
 
-            <Link to={"/delete/" + props.todo._id}> Delete</Link>
+            <Link to={"/todos/" + props.todo._id}> Delete</Link>
         </td>
     </tr>
 )
